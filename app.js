6bb089749d33f560d7752ef16dc203cbb0cc9275
valerio martella <valerio martella>
2016-02-26 22:31:10 +0100

    added alternative and additional parent, not fully supported yet

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var points = parseInt($('.resume').text());
 var pointExpended = 0;
+var pipe = 0;
 
+/*ricorsivamente ritorna il costo di un intero ramo dell'albero dello skilltree*/
 var add_checker = function(current_node) {
 	
 	var parent_node = current_node.parent().parent().parent().children('.inactive-node');
@@ -12,18 +14,16 @@ var add_checker = function(current_node) {
 		return parseInt(current_node.children('.info').children('.cost').text());
 	}
 	
-}
+};
 
-var click_handler = function(evt,current_node,mode) {
-		
-		
+var click_handler = function(evt,current_node) {
+
+
 		$('.dropdown-menu').hide();
 		$('.dropdown-menu').removeClass('active-dropdown-menu');
-		
+
 		var cost = parseInt(current_node.children('.info').children('.cost').text());
-		
-//		alert(mode+' '+current_node.children('a').text());
-		
+
 		if(current_node.hasClass('active-node')) {
 //l'abilità viene rimossa
 			current_node.addClass('inactive-node');
@@ -31,28 +31,40 @@ var click_handler = function(evt,current_node,mode) {
 			pointExpended = pointExpended - cost;
 			$('.resume').text(points-pointExpended);
 			var child_node = current_node.parent().children('ul').children('li').children('.active-node');
-			var child_node_container = current_node.parent().children('ul').children('li');
 
 			if(child_node.length != 0) {
-				child_node.each(function(evt) { click_handler(evt, $(this), mode )})
-//				click_handler(evt,child_node,"remove");
+				child_node.each(function(evt) { click_handler(evt, $(this))});
 			}
 		}
 		else {
 //l'abilità viene comprata
 			var parent_node = current_node.parent().parent().parent().children('.inactive-node');
+			var parent_node_add = current_node.children('.add_parent');
+			var parent_node_alt = current_node.children('.alt_parent');
+			var parent_cost = 0;
+			var add_parent_cost = 0;
 			if(parent_node.length != 0) {
-				if(points-(add_checker(parent_node) + cost + pointExpended) < 0) {
-					alert("Limite dei punti superato");	
+				parent_cost = add_checker(parent_node) + cost + pointExpended;
+				/* controlla la presenza eventuali parenti alternativi e se è possibile aggiungerli*/
+				if(parent_node_add.length != 0)
+				{
+					add_parent_cost = 0;
+					parent_node_alt.each(function(evt) {
+						add_parent_cost = add_parent_cost + add_checker(getElementById($(this).text()))
+					});
+					alert("debug");
+				}
+				if(points-(parent_cost+add_parent_cost) < 0) {
+					alert("Stai chiedendo troppo cittadino, non hai piu' punti.");
 					return;
 				}
 				else {
-					click_handler(evt,parent_node,"add");
+					click_handler(evt,parent_node);
 				}
 			}
 			if(points-(cost + pointExpended) < 0)
 			{
-				alert("Limite dei punti superato");	
+				window.alert("Stai chedendo troppo cittadino, non hai piu' punti.");
 					return;
 			} else {
 				pointExpended = pointExpended + cost;
@@ -61,12 +73,13 @@ var click_handler = function(evt,current_node,mode) {
 				current_node.removeClass('inactive-node');
 			}
 		}
-	}
+	};
 
 var main = function() {
 
+// Gestione delle finestre dropdown delle descrizioni
 	$('.dropdown-toggle').click( function(evt) {
-		
+
 		evt.stopPropagation();
 		var current_description = $(this).parent().find('.dropdown-menu');
 		
@@ -79,12 +92,10 @@ var main = function() {
 			current_description.slideToggle();
 		}
     });
- 	
-	$('.node').click(function (evt) { click_handler(evt,$(this),"begin")});
+ 
+ //gestione dei nodi
+	$('.node').click(function (evt) { click_handler(evt,$(this))});
 	
-	$('.resume').scroll(function() { 
-		$('#FixedDiv').css('top', $(this).scrollTop());
-	});
 };
 
 
